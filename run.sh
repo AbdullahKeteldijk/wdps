ba9caa4519550e0a110c3d0142ac992e20c8056b
rubenhekster <33347314+rubenhekster@users.noreply.github.com>
2018-11-24 17:27:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ ATT=${1:-"WARC-Record-ID"}
 INFILE=${2:-"hdfs:///user/bbkruit/sample.warc.gz"}
 STANFORD=${3:-"C:\Users\ruben\Documents\WDPS_2018\stanford-ner-2018-10-16.zip"}
 
-PYSPARK_PYTHON=/home/wdps1813/bin/python ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --master yarn mainfile.py $ATT $INFILE $STANFORD
+PYSPARK_PYTHON=C:\Users\ruben\Documents\WDPS_2018\pyspark-2.4.0.tar.gz ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit --master yarn mainfile.py $ATT $INFILE $STANFORD
