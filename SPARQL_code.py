da43f6c0cf3e1ae81aaecdcab63edf6b757f9df0
rubehekster <ruben.hekster@hotmail.com>
2018-12-18 11:26:57 +0100

    Update

--- SPARQL_code.py
+++ SPARQL_code.py
@@ -124,6 +124,7 @@ for i in ids:
 		n_total = n_total+n
 		
 for item in scores.keys():
+	pdb.set_trace()
 	replace    		 = item.replace('/','.')
 	replace 		 = replace[1:]
 	scores[replace]  = scores.pop(item)
