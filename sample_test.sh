505f3bb85d14cd0013ffdaae6de61a435480c4dd
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 18:21:22 +0100

    Update sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -11,13 +11,13 @@
 # # Start Workers
 # "${SPARK_HOME}/sbin"/start-slaves.sh
 
-SCRIPT=${1:-"EntityRecognition5.py"}
+# SCRIPT=${1:-"EntityRecognition5.py"}
 # INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 # STANFORD=${3:-"/home/wdps1813/scratch/wdps1813/wdps/stanford-ner-2017-06-09"}
 # OUTFILE=${4:-"folder"}
 
 
 PYSPARK_PYTHON=$(readlink -f $(which python)) ~/../../local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
---master yarn $SCRIPT #$INFILE #$STANFORD $OUTFILE
+--master yarn #$SCRIPT #$INFILE #$STANFORD $OUTFILE
 
 # hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
