8b748886acb59c31a5ecbd8d96ba5a4f2cc1077f
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 16:27:38 +0100

    Update EntityRecognition5.py

--- EntityRecognition5.py
+++ EntityRecognition5.py
@@ -100,7 +100,7 @@ def get_candidate_entities(input, st):
     #
     output = list(set(output))
 
-    with open('Output2/sample-output' +input[0][12:-1]+ '.csv', 'w', newline='') as myfile:
+    with open('folder/sample-output' +input[0][12:-1]+ '.csv', 'w', newline='') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(output)
     print("doc: ", input[0])
