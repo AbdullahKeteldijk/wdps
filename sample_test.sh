71290bd4743beea6d180dd7918c07c396c78d7a6
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 16:43:00 +0100

    Update sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -1,15 +1,15 @@
-if [ -z "${SPARK_HOME}" ]; then
-  export SPARK_HOME="$(cd "`dirname "$0"`"/..; pwd)"
-fi
+# if [ -z "${SPARK_HOME}" ]; then
+#   export SPARK_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+# fi
 
-# Load the Spark configuration
-. "${SPARK_HOME}/sbin/spark-config.sh"
+# # Load the Spark configuration
+# . "${SPARK_HOME}/sbin/spark-config.sh"
 
-# Start Master
-"${SPARK_HOME}/sbin"/start-master.sh
+# # Start Master
+# "${SPARK_HOME}/sbin"/start-master.sh
 
-# Start Workers
-"${SPARK_HOME}/sbin"/start-slaves.sh
+# # Start Workers
+# "${SPARK_HOME}/sbin"/start-slaves.sh
 
 SCRIPT=${1:-"EntityRecognition5.py"}
 INFILE=${2:-"/home/wdps1813/scratch/wdps1813/wdps/data/sample.warc.gz"}
