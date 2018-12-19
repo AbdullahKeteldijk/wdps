57f0efd13faca1c9fb69c3cefc92f3efb2b2f7f6
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 21:46:41 +0100

    Update sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -11,8 +11,8 @@
 # # Start Workers
 # "${SPARK_HOME}/sbin"/start-slaves.sh
 
-SCRIPT=${1:-"EntityRecognition5.py"}
-INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
+# SCRIPT=${1:-"EntityRecognition5.py"}
+# INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 # STANFORD=${3:-"/home/wdps1813/scratch/wdps1813/wdps/stanford-ner-2017-06-09"}
 # OUTFILE=${4:-"folder"}
 
@@ -25,10 +25,16 @@ INFILE=${2:-"hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz"}
 
 # ~/../../local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --class Spark.SparkScript --executor-memory 5g --num-executors 20 --conf spark.memory.fraction=0.8 --conf spark.yarn.am.memory=6g --master yarn --master yarn-cluster --executor-memory 5g --num-executors 20
 
-PYSPARK_PYTHON=$(readlink -f $(python)) ~/../../local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
---executor-memory 5g \
---num-executors 20 \
---conf spark.memory.fraction=0.8 \
---conf spark.yarn.am.memory=6g \
---master yarn-cluster \ 
-$SCRIPT $INFILE
+# PYSPARK_PYTHON=$(readlink -f $(python)) ~/../../local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
+# --executor-memory 5g \
+# --num-executors 20 \
+# --conf spark.memory.fraction=0.8 \
+# --conf spark.yarn.am.memory=6g \
+# --master yarn-cluster \ 
+# $SCRIPT $INFILE
+
+
+time python3 EntityRecognition5.py <(hdfs dfs -cat hdfs:///user/bbkruit/sample.warc.gz | zcat) >
+
+python3 --executor-memory 5g --num-executors 20 --conf spark.memory.fraction=0.8 --conf spark.yarn.am.memory=6g \
+--master yarn-cluster 
