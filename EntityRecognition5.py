a65930c18e0c81b423b4f9685a07f159b8df3c5c
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 18:26:24 +0100

    Update EntityRecognition5.py

--- EntityRecognition5.py
+++ EntityRecognition5.py
@@ -170,7 +170,7 @@ def decode(x, record_attribute):
 # java_path = "C:\Program Files\Java\jre1.8.0_191\binjava.exe"
 # os.environ['JAVAHOME'] = java_path
 
-record_attribute = sys.argv[1]
+record_attribute = 'WARC-Record-ID'
 # Here we use a smaller testfile due to computation time. Use the sample.war.gz for real testing.
 in_file =  'hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz' # 'hdfs:///user/bbkruit/sample.warc.gz' #"/home/wdps1813/scratch/wdps1813/wdps/data/testing.warc.gz"
 stanford =  '/home/wdps1813/scratch/wdps1813/wdps/stanford-ner-2017-06-09'
