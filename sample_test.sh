07399e504688e287e5cac045204417f953e6493b
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 21:50:33 +0100

    Update sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -36,5 +36,5 @@
 
 time python3 EntityRecognition5.py <(hdfs dfs -cat hdfs:///user/bbkruit/sample.warc.gz | zcat) 
 
-python3 --executor-memory 5g --num-executors 20 --conf spark.memory.fraction=0.8 --conf spark.yarn.am.memory=6g \
+python3 --executor-memory 5g --num-executors 40 --conf spark.memory.fraction=0.8 --conf spark.yarn.am.memory=6g \
 --master yarn-cluster 
