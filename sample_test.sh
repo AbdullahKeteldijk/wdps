e4f4d12116d13684fd06168e7179526dd15541b6
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-19 16:44:57 +0100

    Update sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -16,7 +16,7 @@ INFILE=${2:-"/home/wdps1813/scratch/wdps1813/wdps/data/sample.warc.gz"}
 OUTFILE=${3:-"folder"}
 
 
-PYSPARK_PYTHON=$(readlink -f $(which python)) ~/~/~/local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
+PYSPARK_PYTHON=$(readlink -f $(which python)) ~/../../local/spark/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
 --master yarn $SCRIPT $INFILE $OUTFILE
 
 hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
