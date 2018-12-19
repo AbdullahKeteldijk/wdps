69412b48373f92b35179cb338def9b59e2e676d6
rubehekster <ruben.hekster@hotmail.com>
2018-12-19 20:37:29 +0100

    Update

--- elastic_sparql.sh
+++ elastic_sparql.sh
@@ -15,18 +15,11 @@ ES_BIN=$(realpath ~/scratch/elasticsearch-2.4.1/bin/elasticsearch)
 
 >.es_log*
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
-echo "waiting for elasticsearch1 to set up..."
-until [ -n "$ES_NODE1" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
-ES_PID1=$!
+echo "waiting for elasticsearch to set up..."
+until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
+ES_PID=$!
 until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
-echo "elasticsearch1 should be running now on node $ES_NODE1:$ES_PORT (connected to process $ES_PID1)" &
-
-prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
-echo "waiting for elasticsearch2 to set up..."
-until [ -n "$ES_NODE2" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
-ES_PID2=$!
-until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
-echo "elasticsearch2 should be running now on node $ES_NODE2:$ES_PORT (connected to process $ES_PID2)" &
+echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
 
 # python3 elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
@@ -38,10 +31,12 @@ KB_PATH=/home/jurbani/data/motherkb-trident
 
 prun -o .kb_log -v -np 1 $KB_BIN server -i $KB_PATH --port $KB_PORT </dev/null 2> .kb_node &
 echo "waiting 5 seconds for trident to set up..."
-until [ -n "$KB_NODE" ]; do KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)'); done
+#until [ -n "$KB_NODE" ]; do KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)'); done
+until [ -n "$KB_NODE" ]; do KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)') &
+                            KB_NODE2=$(cat .kb_node | grep '^:' | grep -oP '(node...)'); done
 sleep 5
 KB_PID=$!
-echo "trident should be running now on node $KB_NODE:$KB_PORT (connected to process $KB_PID)"
+echo "trident should be running now on node $KB_NODE:$KB_PORT and $KB_NODE2:$KB_PORT   (connected to process $KB_PID)"
 
 ### OLD TRIDENT URL CODE ###
 # KB_PORT=9090
