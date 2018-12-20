cab5e0304bcf066d2d9cc898a22326664bf7c2ae
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-20 09:56:30 +0100

    Add files via upload

--- CSV_Merge.py
+++ CSV_Merge.py
@@ -3,7 +3,7 @@ import pandas as pd
 import csv
 
 
-path =r'C:/Users/klm85310/Documents/WDPS/Output2' # use your path
+path =r'C:/Users/klm85310/Documents/WDPS/Output3' # use your path
 allFiles = glob.glob(path + "/*.csv")
 # frame = pd.DataFrame()
 list_ = []
@@ -24,17 +24,20 @@ for i in range(len(list_)):
     for j in range(len(temp)):
         # print(temp[j].split(','))
         list_2 = temp[j].split(',')
-        ent = list_2[0].replace(" '", "")
+        iden = list_2[0].replace(" '", "")
+        iden = iden.replace("'", "")
+        ent = list_2[1].replace(" '", "")
         ent = ent.replace("'", "")
-        tag = list_2[1].replace(" '", "")
+        tag = list_2[2].replace(" '", "")
         tag = tag.replace("'", "")
-        new_list.append((ent, tag))
+        tag = tag.replace("\n", "")
+        new_list.append((iden, ent, tag))
     # if temp[0] == '' or temp[0] == ' ':
 
     # temp = temp.remove('')
 result = list(set(new_list))
 
-with open("Entities_2.csv", "w") as f:
+with open("Entities_3.csv", "w") as f:
     writer = csv.writer(f,delimiter = '\n')
     writer.writerow(result)
 
