9fe08fadfbaf0755dbd76bc8c836603da6212a3f
rubehekster <ruben.hekster@hotmail.com>
2018-12-21 18:07:09 +0100

    Update

--- EntityRecognition6.py
+++ EntityRecognition6.py
@@ -185,7 +185,7 @@ sc = SparkContext(conf = conf,
             batchSize = 1024)
 
 st = StanfordNERTagger(stanford + '/classifiers/english.all.3class.distsim.crf.ser.gz',
-                       stanford,# + '/stanford-ner.jar',
+                       stanford + '/stanford-ner.jar',
                        encoding='utf-8')
 
 rdd_whole_warc_file = rdd = sc.newAPIHadoopFile(in_file,
