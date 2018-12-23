06e17d03054ca32dd2d92735f6ae7f513dc7036c
Blargon707 <31661901+Blargon707@users.noreply.github.com>
2018-12-23 21:42:30 +0100

    Add files via upload

--- KnowledgeGraph.py
+++ KnowledgeGraph.py
@@ -9,62 +9,78 @@ from nltk.corpus import stopwords
 
 
 def get_top_percent(df, percent):
-    
+'''
+    Gets the specified percentage of most popular words the dataframe
+    measured by number of upvotes
+'''
+
     no_rows = df.shape[0]
-    
+
     one_percent = int(percent * no_rows)
     df_sort = df.sort_values(by=['thumbs_up'], ascending=False)
     df_top_one = df_sort.head(one_percent).copy()
-    
-    
+
+
     return df_top_one
 
 
 
 def get_relation(df, num_iter):
-    
+'''
+    input
+        df: dataframe containing the the words and definitions
+        num_iter: Number of times the process is repeated
+    return
+        dictionary with word as keys and the words from the definitions that are also keys as values
+'''
     df_ = df.values
-    
+
     word = df_[:,0]
     definition = df_[:,4]
-    
+
     stop_words = set(stopwords.words('english'))
-    
+
     dictionary = defaultdict(list)
-    
-    
+
+
     # num_iter =
     for i in range(0, num_iter):
         print('Iteration %s ...' %(i+1))
         for i in range(len(word)):
-            key = word[i]
-            key = key.replace('$$', 'ss')
+            key = str(word[i])
+            key = key.replace('$$', 'ss') # removing weird characters
             label = []
-            
-            
-            text = str(definition[i]).lower()
-            text = re.sub(r'[^\w\s]', '', text)
-            text = text.split()
-            text = [w for w in text if not w in stop_words]
+
+
+            text = str(definition[i]).lower() # definition in lowercase
+            text = re.sub(r'[^\w\s]', '', text) # removes punctuation
+            text = text.split() # splits the text of the definition by spaces
+            text = [w for w in text if not w in stop_words] # removing stopwords
             # print(text)
             for j in range(len(text)):
                 if text[j] in dictionary.keys() and key != text[j] and text[j] not in label:
+                    # if the word is not already in the dictionary and the word is not the same as the key and the label is unique
+                    # then it is added to the list
                     label.append(text[j])
-            # print(text[j])
-            
+
+
             if not label and key in dictionary.keys():
+                # if the label is not empty it will be appended
                 dictionary[key].append(label)
-            # dictionary[key].append(label)
+
             else:
+                # else it will be created
                 dictionary[key] = label
 
-# print(dictionary)
-                    return dictionary
+    # print(dictionary)
+    return dictionary
 
 def drop_empty(df_dict):
-    
+'''
+    Drops all empty values from the dictionary
+'''
     new_dict = {}
-    
+
     for k, v in df_dict.items():
         new_labels = []
         if len(v) > 1:
@@ -74,58 +90,53 @@ def drop_empty(df_dict):
                     new_labels.append(v[i])
             if len(new_labels) > 0:
                 new_dict[k] = new_labels
-    
+
         elif v != [[]]:
             print('Deleted: ', k)
             new_dict[k] = v
-# print()
+            # print()
 
 
-                return new_dict
+    return new_dict
 
 def Dict2Graph(test_dict):
-    
     keys = list(test_dict.keys())
     g = nx.DiGraph()  # maak directed graph aan
-    
-    g.add_nodes_from(keys)   #maak voor elke key een node aan
+
+    g.add_nodes_from(keys)   #Generates a node for each key
     #
-    for i in range(len(keys)):  # voor het aantal keys
-        # print(test_dict[list(test_dict.keys())[i]], test_dict[list(test_dict.keys())[i]] == [[]])
-        # if len( test_dict[list(test_dict.keys())[i]] )>0 :  # als een key minstens 1 label heeft
-        theListOfThisKey=test_dict[keys[i]] # zet deze labels in een list
-        for j in range(len(theListOfThisKey)):     #voor elke item in deze list trek een lijn van onze key naar het item
+    for i in range(len(test_dict)):
+        theListOfThisKey=test_dict[keys[i]] # Puts the labels in a list
+        print(theListOfThisKey)
+        for j in range(len(theListOfThisKey)):     #for each item in this list  we generate an edge from our key to the item
             g.add_edge(keys[i],theListOfThisKey[j])
     #
     #
-    indegrees=[i[1] for i in list(g.in_degree)]   # inkomende aantal pijlen per key
-    sizeOfNodes=[300] * (len(indegrees))   # normaliter zijn de sizes 300 voor elke key
+    indegrees=[i[1] for i in list(g.in_degree)]   # number of incoming edges per key
+    sizeOfNodes = [300] * (len(indegrees))
+    for j in range(len(sizeOfNodes)):
+        sizeOfNodes[j]=sizeOfNodes[j]+indegrees[j]*100
 
-for j in range(len(sizeOfNodes)):
-    sizeOfNodes[j]=sizeOfNodes[j]+indegrees[j]*100   # voor elke inkomende pijl voeg 100 aan grootte toe, deze 100 kun je aanpassen als je dat wilt
-    
-    
-    
     return g, sizeOfNodes
 
 def main():
     pd.set_option('display.max_columns', None)
-    df = pd.read_csv('Urban Dictionary Entries (Part 1 _ 4).csv')
-    
-    df_top_one = get_top_percent(df, 0.01)
-    
-    df_dict = get_relation(df_top_one, 2)
-    
+    df = pd.read_csv('Urban.csv')
+
+    # df_top_one = get_top_percent(df, 0.01)
+
+    df_dict = get_relation(df, 2)
+
     df_dict_clean = drop_empty(df_dict)
     # print(df_rel)
-    
+
     g, sizeOfNodes = Dict2Graph(df_dict_clean)
-    
-    pos = nx.kamada_kawai_layout(g,scale=4)
-    nx.draw(g,pos, with_labels=True, node_size=sizeOfNodes,font_size=6)
+
+    pos = nx.kamada_kawai_layout(g, scale=4)
+    nx.draw(g, pos, with_labels=True, node_size=sizeOfNodes, font_size=6)
     plt.draw()
-    # plt.savefig("/Users/Desktop/graphhhh.png", dpi=1000)  << activeer als je wilt opslaan, dpi=1000 zorgt voor inzoom mogelijkheid
-    
+    plt.savefig("KG1.png", dpi=1000)
+
     plt.show()
 
 if __name__ == '__main__':
